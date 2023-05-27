82e0ff8bdee977247c03e577c17e19149eac1371
Susanne Krkoska <zuzka.krkoska@seznam.cz>
2023-05-27 10:31:04 +0200

    Router added

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import {createRoot} from "react-dom/client";
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <Outlet />
     <Footer />
   </>
+
 );
+
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 import './style.css';
+import {Home} from "./components/Home";
 
 export const API_BASE_URL = 'https://apps.kodim.cz/daweb/leviexpress/api';
 
-createRoot(document.querySelector('#app')).render(<App />, );
+const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <App />,
+            children: [
+                {
+                    path: "/",
+                    element: <Home />
+                },
+                {
+                    path: "/reservation",
+                    element: <h2>Detail jízdenky</h2>,
+                }
+            ]
+        },
+
+    ]
+);
+
+createRoot(document.querySelector('#app')).render(
+    <RouterProvider router={router} />
+);
